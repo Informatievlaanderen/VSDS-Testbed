e345197c9526243de2c7aff2130f7f177f2ff481
XD <xueying_deng@outlook.com>
2023-07-12 18:10:40 +0200

    Add customized service - Log needs to improvement
    Function needs to rewrite

--- test-services/src/main/java/be/vlaanderen/ldes/handlers/RDFComparisonHandler.java
+++ test-services/src/main/java/be/vlaanderen/ldes/handlers/RDFComparisonHandler.java
@@ -1,79 +1,102 @@
 package be.vlaanderen.ldes.handlers;
-
+import be.vlaanderen.ldes.gitb.TestBedLogger;
+import java.io.ByteArrayInputStream;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.springframework.stereotype.Component;
+import org.w3c.dom.Document;
+import org.w3c.dom.*;
 import be.vlaanderen.ldes.gitb.TestBedLogger;
 import com.gitb.core.LogLevel;
-import java.io.StringReader;
-import java.time.OffsetDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 import org.apache.jena.graph.Factory;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.StmtIterator;
 import org.apache.jena.riot.RDFLanguages;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
+import java.io.StringReader;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
 /**
- * Handle SPARQL select queries.
+ * Handle the comparison of two RDF graphs in a XML format.
  */
 @Component
 public class RDFComparisonHandler {
 
-  /**
-   * Compare if two RDF graphs are equal.
-   *
-   * @param inputFirstRDF The first input RDF graph.
-   * @param inputSecondRDF The second input RDF graph.
-   * @return boolean result for the comparison.
-   */
   /** Logger. */
-  private static final Logger LOG = LoggerFactory.getLogger(RDFComparisonHandler.class);
+  private static final Logger LOG = LoggerFactory.getLogger(
+    RelationTimestampValidationHandler.class
+  );
 
-  public Boolean compare(
-    String inputFirstRDF,
-    String inputSecondRDF,
-    String ContentType,
+  /**
+   * Compare two RDF graphs in a XML format.
+   *
+   * @param xml1 The first RDF graph in a XML format.
+   * @param xml2 The second RDF graph in a XML format.
+   * @param logger The logger to log the result of the comparison.
+   * @return True if the two RDF graphs are logically equal, false otherwise.
+   */
+
+  public boolean compareXMLUris(
+    String xml1,
+    String xml2,
     TestBedLogger logger
   ) {
-    var model1 = ModelFactory.createModelForGraph(Factory.createDefaultGraph());
-    var model2 =  ModelFactory.createModelForGraph(Factory.createDefaultGraph());
-    try (var reader = new StringReader(inputFirstRDF)) {
-      model1.read(
-        reader,
-        null,
-        RDFLanguages.contentTypeToLang(ContentType).getName()
-      );
+    LOG.debug("Compare two RDF graphs in XML format");
+    LOG.debug("First string {}", xml1);
+    LOG.debug("First string {}", xml2);
+    try {
+      DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+      DocumentBuilder builder = factory.newDocumentBuilder();
+      Document doc1 = builder.parse(new ByteArrayInputStream(xml1.getBytes()));
+      Document doc2 = builder.parse(new ByteArrayInputStream(xml2.getBytes()));
+      NodeList uriList1 = doc1.getElementsByTagName("uri");
+      NodeList uriList2 = doc2.getElementsByTagName("uri");
+      return compareNodeLists(uriList1, uriList2);
+    } catch (Exception e) {
+      e.printStackTrace();
+      return false;
     }
-    try (var reader = new StringReader(inputSecondRDF)) {
-      model2.read(
-        reader,
-        null,
-        RDFLanguages.contentTypeToLang(ContentType).getName()
-      );
+  }
+
+  private boolean compareNodeLists(NodeList nodeList1, NodeList nodeList2) {
+    if (nodeList1.getLength() != nodeList2.getLength()) {
+      return false;
     }
-    boolean isSame = true;
-    StmtIterator iter1 = model1.listStatements();
-    while (iter1.hasNext()) {
-      if (!model2.contains(iter1.nextStatement())) {
-        isSame = false;
-        break;
+
+    // Iterate over the nodes in nodeList1
+    for (int i = 0; i < nodeList1.getLength(); i++) {
+      Node node1 = nodeList1.item(i);
+      String nodeValue1 = node1.getTextContent();
+
+      // Iterate over the nodes in nodeList2
+      boolean foundMatch = false;
+      for (int j = 0; j < nodeList2.getLength(); j++) {
+        Node node2 = nodeList2.item(j);
+        String nodeValue2 = node2.getTextContent();
+
+        // Compare node values
+        if (nodeValue1.equals(nodeValue2)) {
+          foundMatch = true;
+          break;
+        }
+      }
+
+      // If a match is not found, return false
+      if (!foundMatch) {
+        return false;
       }
     }
 
-    StmtIterator iter2 = model2.listStatements();
-    while (iter2.hasNext()) {
-      if (!model1.contains(iter2.nextStatement())) {
-        isSame = false;
-        break;
-      }
-    }
-
-    return isSame;
+    return true;
   }
 }
